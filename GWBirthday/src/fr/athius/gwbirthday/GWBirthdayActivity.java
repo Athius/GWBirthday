90769538bdc3b0f4d22065f6a5252b46b9e95a7c
Athius <romain.leguay@gmail.com>
2011-12-08 20:03:04 +0100

    Initialization of the awesome project GWBirthday!!!

---
+++ GWBirthday/src/fr/athius/gwbirthday/GWBirthdayActivity.java
@@ -0,0 +1,13 @@
+package fr.athius.gwbirthday;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GWBirthdayActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
